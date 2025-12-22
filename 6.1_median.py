eff0b662bbef3ac93da21c49b267fe9c32b75ada
Sheebah43 <mirsheebahsufi@gmail.com>
2025-12-22 19:57:16 +0530

    Add median function for two sorted arrays
    Implemented a function to find the median of two sorted arrays.

--- 6.1_median.py
+++ 6.1_median.py
@@ -1,30 +1,17 @@
 # Median of 2 sorted arrays 
-import heapq
+def median(array1, array2):
+    merged = sorted(array1 + array2)
+    n=len(merged)
 
-def dijkstra(graph, start):
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0
-    pq = [(0, start)]
-
-    while pq:
-        dist, node = heapq.heappop(pq)
-        if dist > distances[node]:
-            continue
-
-        for neighbor, weight in graph[node]:
-            new_dist = dist + weight
-            if new_dist < distances[neighbor]:
-                distances[neighbor] = new_dist
-                heapq.heappush(pq, (new_dist, neighbor))
-
-    return distances
-
-
+    if n%2==1:
+        return merged [n//2]
+    else:
+        return (merged[n//2-1] + merged[n//2])/2
 if __name__ == "__main__":
-    graph = {
-        'A': [('B', 1), ('C', 4)],
-        'B': [('C', 2), ('D', 5)],
-        'C': [('D', 1)],
-        'D': []
-    }
-    print(dijkstra(graph, 'A'))
+    arr1 = [1,3,9]
+    arr2 = [2,6,9,20]
+    print(median(arr1, arr2)) #1,2,3,6,9,9,20 ->>6
+
+    arr3 = [1, 2]
+    arr4 = [3, 4]
+    print(median(arr3, arr4))  #1,2,3,4 ->>2.5
