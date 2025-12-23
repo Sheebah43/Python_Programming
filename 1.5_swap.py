1857b49968debdcb6441c211298d29841133ccb1
Sheebah43 <mirsheebahsufi@gmail.com>
2025-12-24 00:25:25 +0530

    Refactor variable swapping in 1.5_swap.py
    Refactor variable swapping logic and improve input prompts.

--- 1.5_swap.py
+++ 1.5_swap.py
@@ -1,10 +1,15 @@
-x = int(input("Enter the value of 1st variable\n"))
-y = int(input("Enter the value of 2nd variable\n"))
-print ("x =", x)
-print ("y =", y)
-temp = x
-x = y
-y = temp
-print ("After swapping\n")
-print ("x = ", x)
-print ("y =", y)
+
+if __name__ == "__main__":
+    a = int(input("Enter the value of 1st variable\n"))
+    b = int(input("Enter the value of 2nd variable\n"))
+    print ("a =", a)
+    print ("b =", b)
+    a=int(input("enter num1\n"))
+    b=int(input("enter num2\n"))
+
+    a=a+b
+    b=a-b
+    a=a-b
+    print("After swapping\n")
+    print("num1 =",a)   
+    print("num2 =",b)
